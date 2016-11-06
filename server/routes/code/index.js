91a5cbb2f6fc5850814490ac2ca4b27292a8e0bd
癸两 <guiliang.sgl@alibaba-inc.com>
2016-11-06 23:17:32 +0800

    增加lobby和is-type-of

---
+++ server/routes/code/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by galen on 16/11/6.
+ */
+
+module.exports = function (router, app) {
+    "use strict";
+    router.prefix('/code');
+
+    router.get('/rxjs');
+}
---
+++ server/utils/loading.js
@@ -0,0 +1,9 @@
+/**
+ * 文件加载器
+ *
+ * Created by galen on 16/11/6.
+ */
+
+const debug = require('debug')('woody: loading');
+const is = require('is-type-of');
+const globby = require('globby');
